83711d21921997b47257875331ff7080f16bc273
abressam <amandabressam@gmail.com>
2024-04-07 17:05:12 -0300

    feat: create product button

--- pele-de-pessego-front/src/App.tsx
+++ pele-de-pessego-front/src/App.tsx
@@ -4,12 +4,13 @@ import ProductCard from './components/ProductCard/ProductCard';
 import ProductDescription from './components/ProductDescription/ProductDescription';
 import ProductCounter from './components/ProductCounter/ProductCounter';
 import ProductHeader from './components/ProductHeader/ProductHeader';
+import ProductButton from './components/ProductButton/ProductButton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ProductHeader />
+        <ProductButton />
       </header>
     </div>
   );
--- pele-de-pessego-front/src/components/ProductButton/ProductButton.tsx
+++ pele-de-pessego-front/src/components/ProductButton/ProductButton.tsx
@@ -1,11 +1,19 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
+import { MdOutlineShoppingBag } from "react-icons/md";
+import Button from 'react-bootstrap/Button';
+import { FormattedMessage } from 'react-intl';
 import { ProductButtonWrapper } from './ProductButton.styled';
 
 interface ProductButtonProps {}
 
 const ProductButton: FC<ProductButtonProps> = () => (
  <ProductButtonWrapper data-testid="ProductButton">
-    ProductButton Component
+   <Button>
+      <div className="items">
+      <MdOutlineShoppingBag />
+      <FormattedMessage id="ProductButton.name" defaultMessage="Adicionar ao carrinho" />
+      </div>
+   </Button>
  </ProductButtonWrapper>
 );
 
