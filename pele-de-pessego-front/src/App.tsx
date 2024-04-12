97b055005dc295d543b22869a637cd7c1081d496
Maria Eduarda Lustre <dudalustre35@gmail.com>
2024-04-12 20:21:33 -0300

    feat: create UserForm

--- pele-de-pessego-front/src/App.tsx
+++ pele-de-pessego-front/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
 
 import ProductCard from './components/ProductCard/ProductCard';
+import UserForm from './components/UserForm/UserForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ProductCard />
+        <UserForm />
       </header>
     </div>
   );
