59cf9c14c8ea807ef2e88c8ae949fde543b926d9
abressam <amandabressam@gmail.com>
2024-04-27 11:15:55 -0300

    feat: remove unused imports

--- pele-de-pessego-front/src/App.tsx
+++ pele-de-pessego-front/src/App.tsx
@@ -1,11 +1,4 @@
 import './App.css';
-import Login from './components/Login/Login';
-import MainProduct from './components/MainProduct/MainProduct';
-
-import MainProduct from './components/MainProduct/MainProduct';
-import ProductCard from './components/ProductCard/ProductCard';
-import UserForm from './components/UserForm/UserForm';
-import CustomerForm from './components/CustomerForm/CustomerForm.lazy';
 import Router from './routes'
 
 function App() {
