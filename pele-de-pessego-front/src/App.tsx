f4e1be536c4d44ffb71009fc6677196f63f7fe44
Maria Eduarda Lustre <dudalustre35@gmail.com>
2024-05-02 13:24:42 -0300

    Imports App

--- pele-de-pessego-front/src/App.tsx
+++ pele-de-pessego-front/src/App.tsx
@@ -1,17 +1,12 @@
 import './App.css';
-import Login from './components/Login/Login';
-import MainProduct from './components/MainProduct/MainProduct';
-
-import ProductCard from './components/ProductCard/ProductCard';
-import UserForm from './components/UserForm/UserForm';
-import CustomerForm from './components/CustomerForm/CustomerForm.lazy';
 import Router from './routes'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Router/>
+        <Router/><br/>
       </header>
     </div>
   );
