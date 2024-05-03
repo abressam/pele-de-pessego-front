ab51e73386eb5efb5a00142ad5c3fe6004c4a910
abressam <amandabressam@gmail.com>
2024-05-03 11:47:59 -0300

    feat: change get to post

--- pele-de-pessego-front/src/services/SignInService.ts
+++ pele-de-pessego-front/src/services/SignInService.ts
@@ -2,8 +2,7 @@ import httpCommon from "../common/http-common";
 import SignInData from "../types/SignInData";
 
 const login = (data: SignInData) => {
-    const url = `/session/login?email=${encodeURIComponent(data.email)}&password=${encodeURIComponent(data.password)}`;
-    return httpCommon.get(url);
+    return httpCommon.post<SignInData>("/session/login", data);
 };
 
 const SignInService = {
